74ef40b1f7421d1e386128cf17f842b887c874b9
Kian <kiannaimiroy@gmail.com>
2023-03-06 20:14:02 -0800

    assert, head, and tail all commiting

--- tail.js
+++ tail.js
@@ -10,3 +10,11 @@ assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, 2);
+
+const tail = function(arr1) {
+  return arr1.slice[1, 0];
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
