c6bd709a4513b78b128a7c25e60953c1e7738637
Kian <kiannaimiroy@gmail.com>
2023-03-08 18:47:09 -0800

    ceated a function to count and output letters from a string as an object

---
+++ countLetter.js
@@ -0,0 +1,40 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (string) {
+  let itemsCounted = {}
+  for (let char of string) {
+    if (char === ' ') {
+      continue;
+    }
+    if (itemsCounted[char]) {
+      itemsCounted[char] += 1
+    }
+    else {
+      itemsCounted[char] = 1
+    }
+  }
+  return itemsCounted
+};
+console.log(countLetters('Hey   wanna talk about   it?'))
+
+// const countOnly = function (allItems, itemsToCount) {
+//   const results = {};
+//   for (const item of allItems) {
+//     if (itemsToCount[item]) {
+//       if (results[item]) {
+//         results[item] += 1;
+//       } else {
+//         results[item] = 1;
+//       }
+//       console.log(item)
+//     }
+//   }
+//   return results;
+// }
